ff2c40b99f41b6ad0feba77fe3538c6828dcdca3
nikunjjoshi08 <nikunj.joshi@sculpsoft.com>
2023-04-17 17:09:11 +0530

    Added models | Added routes | 17APR2023

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const connectMongo = require("./db");
-const express = require('express')
+const express = require('express');
 
 connectMongo();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Nikka...!')
+  res.send('Hello Nikka...!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.json(req.body);
+})
+
+module.exports = router;
