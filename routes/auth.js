5ac7d01c8ba51260bad1698658488ef2ff41ef59
nikunjjoshi08 <nikunj.joshi@sculpsoft.com>
2023-04-18 13:03:28 +0530

    Added custom validation | Added server option in scripts | 18APR2023

--- routes/auth.js
+++ routes/auth.js
@@ -6,28 +6,31 @@ const { body, validationResult } = require("express-validator");
 router.post(
   "/create-users",
   [
-    body("name", "Enater valid name").isLength({ min: 5 }),
-    body("email").isEmail(),
-    body("password").isLength({ min: 5 }),
+    body("name", "Enater valid name").isLength({ min: 5 }).trim(),
+    body("email")
+      .isEmail()
+      .custom(async (value) => {
+        if (await User.findOne({ email: value })) {
+          throw new Error("User already exists");
+        }
+      }),
+    body("password").trim().isLength({ min: 5 }),
   ],
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      return res.json(user);
+    } catch (error) {
+      res.status(500).json({ errro: error.message });
     }
-
-    let user = await User.findOne({ email: req.body.email });
-
-    if (user) {
-      return res.status(409).json({ error: "User already exists" });
-    }
-
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.json(user);
   }
 );
 
