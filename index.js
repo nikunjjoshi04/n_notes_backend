2fda3d6597de62e45763ec492abec7256390a00f
nikunjjoshi08 <nikunj.joshi@sculpsoft.com>
2023-04-21 11:58:33 +0530

    Allow cross access | 21APR2023

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const connectMongo = require("./db");
 const express = require("express");
+var cors = require('cors')
 
 connectMongo();
 
 const app = express();
 const port = 5000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
